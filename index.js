39a7627785c674c1e9f2810c8565087ff1ed26ed
Matthew Kevins <mmkevins@yahoo.com>
2015-06-09 23:10:31 -0400

    Add start-game modal; style modals; add css reset;

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 $(function () {
-  var context = $('#canvas')[0].getContext("2d");
-  var game = new Asteroids.Game({
-    dimX: window.innerWidth,
-    dimY: window.innerHeight,
-    livesCount: 3
+  $('.start').one('click', function () {
+    $('.modal-wrapper').hide();
+    $('.start-game').hide();
+    var context = $('#canvas')[0].getContext("2d");
+    var game = new Asteroids.Game({
+      dimX: window.innerWidth,
+      dimY: window.innerHeight,
+      livesCount: 3
+    });
+    var gameView = new Asteroids.GameView(game, context);
+    gameView.start();
+
+    var resizeTimeoutId;
+    $(window).resize(function (event) {
+      clearTimeout(resizeTimeoutId);
+      resizeTimeoutId = setTimeout(throttledResize, 100);
+    });
+
+    function throttledResize (event) {
+      //console.log("window resized");
+      var width = $(window).width();
+      var height = $(window).height();;
+      console.log("width: " + width);
+      console.log("height: " + height);
+
+      context.canvas.width = width;
+      context.canvas.height = height;
+      game.dimX = width;
+      game.dimY = height;
+    }
+
+    $(window).resize();
   });
-  var gameView = new Asteroids.GameView(game, context);
-  gameView.start();
-
-  var resizeTimeoutId;
-  $(window).resize(function (event) {
-    clearTimeout(resizeTimeoutId);
-    resizeTimeoutId = setTimeout(throttledResize, 100);
-  });
-
-  function throttledResize (event) {
-    //console.log("window resized");
-    var width = $(window).width();
-    var height = $(window).height();;
-    console.log("width: " + width);
-    console.log("height: " + height);
-
-    context.canvas.width = width;
-    context.canvas.height = height;
-    game.dimX = width;
-    game.dimY = height;
-  }
-
-  $(window).resize();
 });
